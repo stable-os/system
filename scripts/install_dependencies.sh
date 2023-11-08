9f40c9bf714f38b7bc854d565f62c439853f2aef
piano1029 <46605242+piano1029@users.noreply.github.com>
2023-11-08 21:05:55 +0100

    Remove Podman

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,2 @@
 sudo apt-get update
-sudo apt-get install ostree podman
-
-# Setup podman socket and allow non-root users to use it
-sudo podman system service unix:///podman.sock --log-level=debug --time=50
-sudo chmod 666 /podman.sock
+sudo apt-get install ostree
