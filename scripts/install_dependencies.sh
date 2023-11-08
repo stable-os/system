bb9e8238ddc2069947d4a2d55b05f662eb72193e
piano1029 <46605242+piano1029@users.noreply.github.com>
2023-11-08 21:03:44 +0100

    Setup Podman

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,2 +1,6 @@
 sudo apt-get update
-sudo apt-get install ostree
+sudo apt-get install ostree podman
+
+# Setup podman socket and allow non-root users to use it
+podman system service unix:///podman.sock --log-level=debug --time=50
+sudo chmod 666 /podman.sock
