83302d6a32198486f40cbe5320a2e78d3b3421f4
piano1029 <46605242+piano1029@users.noreply.github.com>
2023-11-08 21:04:45 +0100

    Run podman system service step with sudo

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,5 +2,5 @@ sudo apt-get update
 sudo apt-get install ostree podman
 
 # Setup podman socket and allow non-root users to use it
-podman system service unix:///podman.sock --log-level=debug --time=50
+sudo podman system service unix:///podman.sock --log-level=debug --time=50
 sudo chmod 666 /podman.sock
