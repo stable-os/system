0177365fb7353d879a277c1a0fb2727171f03b15
piano1029 <46605242+piano1029@users.noreply.github.com>
2023-11-08 21:38:22 +0100

    Fix sh invocation

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,4 +2,4 @@ sudo apt-get update
 sudo apt-get install ostree
 
 # Rust
-sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -y
+sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
