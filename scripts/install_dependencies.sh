f9cf7d0073dba44bdd71dc8d3719bb8000ee59a4
piano1029 <46605242+piano1029@users.noreply.github.com>
2023-11-08 21:34:35 +0100

    Fix Rust installer

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,4 +2,4 @@ sudo apt-get update
 sudo apt-get install ostree
 
 # Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -y
