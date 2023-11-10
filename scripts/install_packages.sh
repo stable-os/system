e3cc3f0719017cc63223505724bdcd619c206dc1
piano1029 <46605242+piano1029@users.noreply.github.com>
2023-11-10 14:33:14 +0100

    Fix package_name variable

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -3,9 +3,9 @@ ARCH=$(uname -m)
 
 for package in packages/package_*
 do
-    package_name=$(echo $package | sed 's/package_//')
+    package_name=$(echo $package | sed 's/packages\/package_//')
     echo $package_name as stable-os/$ARCH/$package_name
-    ostree --repo=$BUILD_REPO commit -b stable-os/$ARCH/$package_name --tree=tar=$package/out.tar.gz
+    ostree --repo=$BUILD_REPO commit -b stable-os/$ARCH/$package_name --tree=tar=packages/$package/out.tar.gz
 done
 
 rm -rf packages
