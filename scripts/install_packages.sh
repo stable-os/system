4d21a444b4f992ba59d789197d35f53349c1952c
piano1029 <46605242+piano1029@users.noreply.github.com>
2023-11-10 14:43:45 +0100

    Fix double packages in path

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -5,7 +5,7 @@ for package in packages/package_*
 do
     package_name=$(echo $package | sed 's/packages\/package_//')
     echo $package_name as stable-os/$ARCH/$package_name
-    ostree --repo=$BUILD_REPO commit -b stable-os/$ARCH/$package_name --tree=tar=packages/$package/out.tar.gz
+    ostree --repo=$BUILD_REPO commit -b stable-os/$ARCH/$package_name --tree=tar=$package/out.tar.gz
 done
 
 rm -rf packages
