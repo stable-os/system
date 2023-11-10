23271ceb1a4c0a73ccb22795dd120fbcc2f0d580
piano1029 <46605242+piano1029@users.noreply.github.com>
2023-11-10 13:54:45 +0100

    Fix sed replace string

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -3,7 +3,7 @@ ARCH=$(uname -m)
 
 for package in packages/package_*
 do
-    package_name=$(echo $package | sed 's/packages\/package_//')
+    package_name=$(echo $package | sed 's/package_//')
     echo $package_name
     ostree --repo=$BUILD_REPO commit -b stable-os/$ARCH/$package_name --tree=tar=packages/$package/out.tar.gz
 done
