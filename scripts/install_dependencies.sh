d7a4179f825baa4c1e966774d4e399c462f0e3cf
piano1029 <46605242+piano1029@users.noreply.github.com>
2023-11-11 13:20:30 +0100

    Fix cargo install

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,4 +4,4 @@ sudo apt-get install ostree
 # Rust
 sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
-sudo cargo install ostree-ext-cli
+$HOME/.cargo/bin/cargo install ostree-ext-cli
