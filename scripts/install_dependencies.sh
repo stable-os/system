3b52544a969106c22d45e23907b030706b393ef1
piano1029 <46605242+piano1029@users.noreply.github.com>
2023-11-11 15:53:31 +0100

    Fix ostree-ext-cli

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,3 +4,5 @@ sudo apt-get install ostree skopeo
 # Rust
 sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
+
+chmod +x ./ostree-ext-cli/ostree-ext-cli
