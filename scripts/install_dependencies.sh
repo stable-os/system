f576923b1a322eb00b7d0f7f4c94cdee3a5c7d6f
piano1029 <46605242+piano1029@users.noreply.github.com>
2023-11-11 16:49:46 +0100

    Add go-md2man

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install ostree libgpgme-dev libassuan-dev libbtrfs-dev libdevmapper-dev pkg-config
+sudo apt-get install ostree libgpgme-dev libassuan-dev libbtrfs-dev libdevmapper-dev pkg-config go-md2man
 
 # Rust
 sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
