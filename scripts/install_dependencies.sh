17c9fff52e850e3d6b6b4fe19e7b131715b1fe22
piano1029 <46605242+piano1029@users.noreply.github.com>
2023-11-11 16:12:15 +0100

    Build Skopeo from source

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,11 @@
 sudo apt-get update
-sudo apt-get install ostree skopeo
+sudo apt-get install ostree libgpgme-dev libassuan-dev libbtrfs-dev libdevmapper-dev pkg-config
 
 # Rust
 sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 
 chmod +x ./ostree-ext-cli/ostree-ext-cli
+
+git clone https://github.com/containers/skopeo $GOPATH/src/github.com/containers/skopeo
+cd $GOPATH/src/github.com/containers/skopeo && DISABLE_DOCS=1 make bin/skopeo && sudo make install
