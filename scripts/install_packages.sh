dde22de1a7dba62b7b58ba5a9d6b62346a367d89
piano1029 <46605242+piano1029@users.noreply.github.com>
2023-12-12 19:58:24 +0100

    Temporarily remove audit

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -108,11 +108,11 @@ for package in bash \
     libxml2 \
     libxslt \
     swig \
-    audit \
-    perl; do
+    \
+    perl; do # audit \
 
     shouldusestableosbuiltpackageinstead=false
-    for usestableosbuiltpackageinstead in flit-core ninja wheel meson asciidoc which linux-api-headers pkg-builder bash libcap libpcre2 ncurses gmp mpc mpfr tar zlib gzip curl openssl p11-kit make-ca grep gawk readline libffi libtasn1 findutils bzip2 xz attr autoconf automake bc bison check diffutils e2fsprogs expat file flex gdbm gettext gperf groff iana-etc kbd less libelf libpipeline libtool libxcrypt m4 make manpages patch perl pkgconf procps python3 shadow sysklogd tcl texinfo timezonedata utillinux xmlparser glib libarchive libpgpme libgpg-error avahi dbus libassuan wget essential-files libdaemon libaio libxslt libxml2 skopeo swig audit; do
+    for usestableosbuiltpackageinstead in flit-core ninja wheel meson asciidoc which linux-api-headers pkg-builder bash libcap libpcre2 ncurses gmp mpc mpfr tar zlib gzip curl openssl p11-kit make-ca grep gawk readline libffi libtasn1 findutils bzip2 xz attr autoconf automake bc bison check diffutils e2fsprogs expat file flex gdbm gettext gperf groff iana-etc kbd less libelf libpipeline libtool libxcrypt m4 make manpages patch perl pkgconf procps python3 shadow sysklogd tcl texinfo timezonedata utillinux xmlparser glib libarchive libpgpme libgpg-error avahi dbus libassuan wget essential-files libdaemon libaio libxslt libxml2 skopeo swig; do
         if [ "$package" = "$usestableosbuiltpackageinstead" ]; then
             shouldusestableosbuiltpackageinstead=true
         fi
