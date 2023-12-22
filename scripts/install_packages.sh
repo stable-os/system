9a710e67ed1ad3a3034ac2869210e66a07b27be6
piano1029 <46605242+piano1029@users.noreply.github.com>
2023-12-22 22:19:14 +0100

    Add xmlto

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -113,10 +113,11 @@ for package in bash \
     pam \
     markupsafe \
     jinja2 \
+    xmlto \
     perl; do
 
     shouldusestableosbuiltpackageinstead=false
-    for usestableosbuiltpackageinstead in flit-core ninja wheel meson asciidoc which linux-api-headers pkg-builder bash libcap libpcre2 ncurses gmp mpc mpfr tar zlib gzip curl openssl p11-kit make-ca grep gawk readline libffi libtasn1 findutils bzip2 xz attr autoconf automake bc bison check diffutils e2fsprogs expat file flex gdbm gettext gperf groff iana-etc kbd less libelf libpipeline libtool libxcrypt m4 make manpages patch perl pkgconf procps python3 shadow sysklogd tcl texinfo timezonedata utillinux xmlparser glib libarchive libpgpme libgpg-error avahi dbus libassuan wget essential-files libdaemon libaio libxslt libxml2 skopeo swig audit fuse pam markupsafe jinja2; do
+    for usestableosbuiltpackageinstead in flit-core ninja wheel meson asciidoc which linux-api-headers pkg-builder bash libcap libpcre2 ncurses gmp mpc mpfr tar zlib gzip curl openssl p11-kit make-ca grep gawk readline libffi libtasn1 findutils bzip2 xz attr autoconf automake bc bison check diffutils e2fsprogs expat file flex gdbm gettext gperf groff iana-etc kbd less libelf libpipeline libtool libxcrypt m4 make manpages patch perl pkgconf procps python3 shadow sysklogd tcl texinfo timezonedata utillinux xmlparser glib libarchive libpgpme libgpg-error avahi dbus libassuan wget essential-files libdaemon libaio libxslt libxml2 skopeo swig audit fuse pam markupsafe jinja2 xmlto; do
         if [ "$package" = "$usestableosbuiltpackageinstead" ]; then
             shouldusestableosbuiltpackageinstead=true
         fi
