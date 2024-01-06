aa658d75cbb130ebe7c42953c1bea8afe3ccdfd7
piano1029 <46605242+piano1029@users.noreply.github.com>
2024-01-06 18:15:01 +0100

    Increase iso build disk space

---
+++ scripts/build_iso.sh
@@ -0,0 +1,11 @@
+make-ca -g
+
+skopeo copy docker://ghcr.io/stable-os/stable-os-bootable:latest dir:/tmp/filesystemimage --dest-decompress
+
+mkdir /tmp/filesystemimage_decompressed && tar -xf $(file --mime-type /tmp/filesystemimage/* | awk -F': ' '$2=="application/x-tar"{print $1}' | head -n 1) -C /tmp/filesystemimage_decompressed
+
+rm -rf /tmp/filesystemimage_decompressed/sysroot
+
+grub-mkrescue -o /tmp/grub-rescue.iso /tmp/filesystemimage_decompressed
+
+cp /tmp/grub-rescue.iso /shareddir/grub-rescue.iso
