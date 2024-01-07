235398c46de8263da9a2fb56a3c3531ea17512d5
piano1029 <46605242+piano1029@users.noreply.github.com>
2024-01-07 21:53:24 +0100

    Prepare for squashfs livecd

--- scripts/build_iso.sh
+++ scripts/build_iso.sh
@@ -2,7 +2,7 @@ make-ca -g
 
 skopeo copy docker://ghcr.io/stable-os/stable-os-bootable:latest dir:/tmp/filesystemimage --dest-decompress
 
-mkdir -pv /tmp/filesystemimage_decompressed/mnt/{lower,upper} && tar -xf $(file --mime-type /tmp/filesystemimage/* | awk -F': ' '$2=="application/x-tar"{print $1}' | head -n 1) -C /tmp/filesystemimage_decompressed/mnt/lower
+mkdir -pv /tmp/filesystemimage_decompressed && tar -xf $(file --mime-type /tmp/filesystemimage/* | awk -F': ' '$2=="application/x-tar"{print $1}' | head -n 1) -C /tmp/filesystemimage_decompressed
 
 # this should get mounted later on by fstab i think
 mkdir -pv /tmp/filesystemimage_decompressed/{proc,tmp,etc,boot}
@@ -13,17 +13,20 @@ cp -r /tmp/filesystemimage_decompressed/mnt/lower/boot/* /tmp/filesystemimage_de
 # create the fstab file
 cat >/tmp/filesystemimage_decompressed/etc/fstab <<"EOF"
 # <file system> <mount point>   <type>  <options>       <dump>  <pass>
-tmpfs           /mnt/upper      tmpfs   defaults        0       0
 tmpfs           /tmp            tmpfs   defaults        0       0
-
-overlay         /               overlay defaults,lowerdir=/mnt/lower,upperdir=/mnt/upper,workdir=/mnt/work 0 0
-
-proc            /proc           proc    defaults        0       0
+/dev/sr0        /media/cdrom0   udf,iso9660 user,noauto     0       0
+/media/cdrom0/image.squashfs / squashfs ro,loop 0 0
 EOF
 
 rm -rf /tmp/filesystemimage_decompressed/sysroot
 
-grub-mkrescue -o /tmp/grub-rescue.iso /tmp/filesystemimage_decompressed
+# create squashfs image
+mksquashfs /tmp/filesystemimage_decompressed /tmp/filesystemimage_decompressed.squashfs -comp xz -Xbcj x86 -b 1M -noappend
+
+mkdir -pv /tmp/livecd
+mv -v /tmp/filesystemimage_decompressed.squashfs /tmp/livecd/image.squashfs
+
+grub-mkrescue -o /tmp/grub-rescue.iso /tmp/livecd
 
 cp /tmp/grub-rescue.iso /shareddir/grub-rescue.iso
 
