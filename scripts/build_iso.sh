603a3a8e1edc0f087fe6ad0048debef7c960e245
piano1029 <46605242+piano1029@users.noreply.github.com>
2024-01-07 18:14:55 +0100

    Fix iso

--- scripts/build_iso.sh
+++ scripts/build_iso.sh
@@ -5,14 +5,10 @@ skopeo copy docker://ghcr.io/stable-os/stable-os-bootable:latest dir:/tmp/filesy
 mkdir -pv /tmp/filesystemimage_decompressed/mnt/{lower,upper} && tar -xf $(file --mime-type /tmp/filesystemimage/* | awk -F': ' '$2=="application/x-tar"{print $1}' | head -n 1) -C /tmp/filesystemimage_decompressed/mnt/lower
 
 # this should get mounted later on by fstab i think
-mkdir -pv /tmp/filesystemimage_decompressed/{proc}
+mkdir -pv /tmp/filesystemimage_decompressed/{proc,tmp,etc,boot}
 
-# the boot stuff is in /mnt/lower, so this should be symlinked to /boot
-# ln -svf /mnt/lower/boot /tmp/filesystemimage_decompressed/boot
-# but because grub also wants to be in there all files have to be symlinked separately
-for file in $(ls /tmp/filesystemimage_decompressed/mnt/lower/boot); do
-    ln -svf /mnt/lower/boot/$file /tmp/filesystemimage_decompressed/boot/$file
-done
+# the boot stuff is in /mnt/lower, so this should be copied to /boot
+cp -r /tmp/filesystemimage_decompressed/mnt/lower/boot/* /tmp/filesystemimage_decompressed/boot
 
 # create the fstab file
 cat >/tmp/filesystemimage_decompressed/etc/fstab <<"EOF"
@@ -21,6 +17,8 @@ tmpfs           /mnt/upper      tmpfs   defaults        0       0
 tmpfs           /tmp            tmpfs   defaults        0       0
 
 overlay         /               overlay defaults,lowerdir=/mnt/lower,upperdir=/mnt/upper,workdir=/mnt/work 0 0
+
+proc            /proc           proc    defaults        0       0
 EOF
 
 rm -rf /tmp/filesystemimage_decompressed/sysroot
