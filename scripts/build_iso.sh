6ede887bbb6e22229ce155c33a1a8b6523352e34
piano1029 <46605242+piano1029@users.noreply.github.com>
2024-01-07 15:58:11 +0100

    Create overlay and tmpfs filesystems

--- scripts/build_iso.sh
+++ scripts/build_iso.sh
@@ -2,7 +2,22 @@ make-ca -g
 
 skopeo copy docker://ghcr.io/stable-os/stable-os-bootable:latest dir:/tmp/filesystemimage --dest-decompress
 
-mkdir /tmp/filesystemimage_decompressed && tar -xf $(file --mime-type /tmp/filesystemimage/* | awk -F': ' '$2=="application/x-tar"{print $1}' | head -n 1) -C /tmp/filesystemimage_decompressed
+mkdir -pv /tmp/filesystemimage_decompressed/mnt/{lower,upper} && tar -xf $(file --mime-type /tmp/filesystemimage/* | awk -F': ' '$2=="application/x-tar"{print $1}' | head -n 1) -C /tmp/filesystemimage_decompressed/mnt/lower
+
+# this should get mounted later on by fstab i think
+mkdir -pv /tmp/filesystemimage_decompressed/{proc}
+
+# the boot stuff is in /mnt/lower, so this should be symlinked to /boot
+ln -svf /mnt/lower/boot /tmp/filesystemimage_decompressed/boot
+
+# create the fstab file
+cat >/tmp/filesystemimage_decompressed/etc/fstab <<"EOF"
+# <file system> <mount point>   <type>  <options>       <dump>  <pass>
+tmpfs           /mnt/upper      tmpfs   defaults        0       0
+tmpfs           /tmp            tmpfs   defaults        0       0
+
+overlay         /               overlay defaults,lowerdir=/mnt/lower,upperdir=/mnt/upper,workdir=/mnt/work 0 0
+EOF
 
 rm -rf /tmp/filesystemimage_decompressed/sysroot
 
