ea7574608cf2c31f3f1b5612c58ccdaa6c4d38fc
piano1029 <46605242+piano1029@users.noreply.github.com>
2024-03-31 18:01:02 +0200

    Add dmsquash-live dracut module

--- scripts/build_iso.sh
+++ scripts/build_iso.sh
@@ -39,7 +39,7 @@ chroot /tmp/filesystemimage_decompressed /usr/bin/bash <<"EOT"
 KERNEL_VERSION=$(find /lib/modules -maxdepth 1 -type d -printf "%f" | sed 's/modules//')
 echo KERNEL VERSION: $KERNEL_VERSION
 mkdir -pv /var/tmp
-dracut /tmp/initramfs.img "$KERNEL_VERSION"
+dracut --add dmsquash-live /tmp/initramfs.img "$KERNEL_VERSION"
 rm -rf /var/tmp
 
 systemctl preset-all
